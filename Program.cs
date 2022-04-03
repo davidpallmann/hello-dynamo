1c74c0272e0f802e8ba39611d6357d3a7d0794c9
David Pallmann <dpallmann@hotmail.com>
2022-04-03 16:12:27 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+
+RegionEndpoint region = RegionEndpoint.USWest2;
+
+var client = new AmazonDynamoDBClient(region);
+Table table = Table.LoadTable(client, "recipe");
+
+if (args.Length == 2 && args[0] == "put")
+{
+    // put [json-filename] - store a recipe
+    var name = args[1];
+    var json = File.ReadAllText(name);
+    var recipe = Document.FromJson(json);
+    Console.WriteLine($"Putting document category:{recipe["category"]} name:{recipe["name"]} to recipe table");
+    var response = await table.PutItemAsync(recipe);
+}
+else if (args.Length == 3 && args[0] == "get")
+{
+    // get [category] [name] - retrieve and display a recipe
+    var partitionKey = args[1];
+    var sortKey = args[2];
+    var recipe = await table.GetItemAsync(partitionKey, sortKey);
+    if (recipe == null)
+    {
+        Console.WriteLine("Recipe not found");
+    }
+    else
+    {
+        Console.WriteLine($"Recipe: {recipe["name"]}");
+        Console.WriteLine($"Category: {recipe["category"]}");
+        Console.WriteLine($"Link: {recipe["link"]}");
+
+        Console.WriteLine($"Intro: {recipe["intro"]}");
+        Console.WriteLine();
+
+        Console.WriteLine("Ingredients");
+        foreach (var ingredient in recipe["ingredients"].AsListOfDynamoDBEntry())
+        {
+            Console.WriteLine($"{ingredient}");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Instructions");
+        int number = 1;
+        foreach (var step in recipe["instructions"].AsListOfDynamoDBEntry())
+        {
+            Console.WriteLine($"{number++}. {step}");
+        }
+    }
+}
+else if (args.Length==1 && args[0] == "list")
+{
+    // list - list recipes
+    var filter = new ScanFilter();
+    filter.AddCondition("name", ScanOperator.IsNotNull);
+
+    var scanConfig = new ScanOperationConfig()
+    {
+        Filter = filter,
+        Select = SelectValues.SpecificAttributes,
+        AttributesToGet = new List<string> { "category", "name", "link" }
+    };
+    Search search = table.Scan(scanConfig);
+    List<Document> matches;
+    do
+    {
+        matches = await search.GetNextSetAsync();
+        foreach (var match in matches)
+        {
+            Console.WriteLine($"{match["category"],-20} {match["name"],-40} {match["link"]}");
+        }
+    } while (!search.IsDone);
+}
+else if (args.Length==2 && args[0] == "list")
+{
+    // list [category] - list recipes in a category
+    var filter = new QueryFilter();
+    filter.AddCondition("category", ScanOperator.Equal, args[1]);
+
+    var filterConfig = new QueryOperationConfig()
+    {
+        Filter = filter,
+        Select = SelectValues.SpecificAttributes,
+        AttributesToGet = new List<string> { "category", "name", "link" }
+    };
+    Search search = table.Query(filterConfig);
+    List<Document> matches;
+    do
+    {
+        matches = await search.GetNextSetAsync();
+        foreach (var match in matches)
+        {
+            Console.WriteLine($"{match["category"],-20} {match["name"],-40} {match["link"]}");
+        }
+    } while (!search.IsDone);
+}
+else
+{
+    Console.WriteLine("To store a recipe:              dotnet run -- put [jsonfile]");
+    Console.WriteLine("To list all recipes:            dotnet run -- list");
+    Console.WriteLine("To list recipes in a category:  dotnet run -- list [category]");
+    Console.WriteLine("To retrieve a recipe:           dotnet run -- get [category] [name]");
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Hello, DynamoDB (Document Model)!
+
+This is the code project for the [Hello, DynamoDB! (Document Model)](https://davidpallmann.hashnode.dev/hello-dynamodb-document-model) blog post. 
+
+This episode: Amazon DynamoDB and the document model. In this Hello, Cloud blog series, we're covering the basics of AWS cloud services for newcomers who are .NET developers. If you love C# but are new to AWS, or to this particular service, this should give you a jumpstart.
+
+In this post we'll explain NoSQL databases, introduce DynamoDB, and use it in a "Hello, Cloud" .NET program to store and retrieve JSON documents. We'll do this step-by-step, making no assumptions other than familiarity with C# and Visual Studio. We're using Visual Studio 2022 and .NET 6
+
+## Our Hello, DynamoDB Project
+
+We will use DynamoDB to store and retrieve recipes in the form of JSON documents. We will first create a DynamoDB table in the AWS console. Then, we'll write a .NET program that can put, list, and get documents using the AWS SDK for .NET Document API for DynamoDB
+
+See the blog post for the tutorial to create this project and run it on AWS.
+
